915bd70961d4db04877d6aa4199520f708b636f1
hungtt <hungtt@gvn-technology.vn>
2021-07-21 16:49:01 +0700

    #4 - Creating a Service Provider

---
+++ src/PaymentPackageServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace ToanHung94mt3\PaymentPackage;
+
+use Illuminate\Support\ServiceProvider;
+
+class PaymentPackageServiceProvider extends ServiceProvider
+{
+  public function register()
+  {
+    //
+  }
+
+  public function boot()
+  {
+    //
+  }
+}
